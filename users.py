972241589c6cdfc833d05a1b6342b2b82fd840bf
Shlomi Atar <shlomiatar@gmail.com>
2016-07-02 19:06:02 +0300

    REDIS FOR THE WINNNN

--- users.py
+++ users.py
@@ -15,6 +15,7 @@ def user_id_for_email(email):
     user_id = redis_pool.get('users.ids.{}'.format(email))
 
     if not user_id:
-        user_id = redis_pool.set('users.ids.{}'.format(email), uuid4())
+        user_id = uuid4()
+        redis_pool.set('users.ids.{}'.format(email), user_id)
 
     return user_id
