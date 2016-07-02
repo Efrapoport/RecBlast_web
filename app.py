df37c16698ea4773bbe609e4ddf862415783a2eb
Shlomi Atar <shlomiatar@gmail.com>
2016-07-02 13:38:18 +0300

    Initial clean import

---
+++ app.py
@@ -0,0 +1,247 @@
+# import os
+from uuid import uuid4
+
+from flask import (Flask, render_template, redirect,
+                   url_for, request, flash)
+from werkzeug.utils import secure_filename
+
+import csv_transformer
+import taxa_to_taxid
+import utils
+from utils import *
+
+app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+UPLOAD_FOLDER = 'r"C:\Users\Efrat\PycharmProjects\recblast\uploaded_files\"'
+ALLOWED_EXTENSIONS = {'txt', 'csv'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['SECRET_KEY'] = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+try:
+    ip = request.remote_addr
+    print ip
+except:
+    ip = "localhost"
+    print ip
+
+
+@app.route('/index')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/documentation')
+def documentation():
+    return render_template('documentation.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/downloads')
+def downloads():
+    return render_template('downloads.html')
+
+
+@app.route('/server')
+def server():
+    user_id = str(uuid4())  # should I move to somewhere else?
+    return render_template("server.html", user_id=user_id)
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+def send_job_to_backend(value_list):
+    try:
+        # sending to the backend main
+        return True
+    except:
+        return False
+
+
+def validate_data(value_list):
+    print value_list
+    error_list = []
+
+    # check email
+    email = value_list[9]
+    if utils.check_if_email_exists(email):
+        error_list.append("There's already a job running for this email: %s" % email)
+
+    # check if files/lists are not empty:
+    gene_file_path = value_list[5]
+
+    if not exists_not_empty(gene_file_path):
+        error_list.append("No gene list or gene file provided!")
+
+    taxa_file = value_list[6]
+    if not exists_not_empty(taxa_file):
+        error_list.append("No taxa list or gene file provided!")
+
+    # taxa database
+    # script folder
+    script_folder = "/groups/igv/moranne/scripts/RecBlast_AWS/"  # TODO: Change to a fixed path
+    tax_db = os.path.join(script_folder, "DB/taxdump/tax_names.txt")
+    # parsing and creating taxa files and parameters:
+    tax_name_dict = taxa_to_taxid.create_tax_dict(tax_db)
+    tax_id_dict = dict((v, k) for k, v in tax_name_dict.iteritems())  # the reverse dict
+
+    reference_taxa = value_list[7]
+    # validate reference taxa!
+    # processing original species
+    if is_number(reference_taxa):  # already a tax_id
+        org_tax_id = reference_taxa
+        try:
+
+            origin_species = tax_id_dict[org_tax_id]
+        except KeyError:
+            error_list.append("Unknown tax id: {}!".format(org_tax_id))
+            origin_species = ""
+            # debug("Tax id given: {0}, which is {1}, no need to convert.".format(org_tax_id, origin_species))
+    else:  # it's a tax name
+        origin_species = reference_taxa
+        # convert it to tax id and write it to a file
+        try:
+            org_tax_id = tax_name_dict[origin_species.capitalize()]
+        except KeyError:
+            error_list.append("Unknown taxon name: {}!".format(origin_species))
+            org_tax_id = ""
+            # debug("Tax name given: {0}, which is tax_id {1}, converted. Saved original tax_id to {2}".format(
+            #     origin_species, org_tax_id, original_taxa_file))
+    # left with (org_tax_id, origin_species)
+
+    #   validate gene list
+    # parsing the genes_csv if it's a csv, and transforming it if it's a gene list file
+    is_csv = False  # by default
+    with open(gene_file_path, 'r') as gene_file:
+        for line in gene_file:
+            if line.find(',') != -1:
+                is_csv = True
+                break
+    if is_csv:
+        csv_path = gene_file_path  # hope it's a good and valid file...
+        # TODO: don't do this now
+        error_list.append("It's csv, we don't support it now!")
+    else:  # if the csv is not provided, create it from a gene file
+        if org_tax_id == "":
+            error_list.append("The gene_list could not be converted. Please check and upload a valid file.")
+            csv_path = ""
+        else:
+            conversion_result = csv_transformer.gene_file_to_csv(gene_file_path, org_tax_id)
+            if conversion_result[0]:
+                csv_path = conversion_result[1]  # quits if no input could be converted
+            else:
+                error_list.append("The gene_list could not be converted. Please check and upload a valid file.")
+                csv_path = ""
+
+    # validate taxa list
+    # converting taxa names list
+    (taxa_list_file, bad_tax_list, conversion_succeeded) = taxa_to_taxid.convert_tax_to_taxid(tax_name_dict, taxa_file)
+    if conversion_succeeded:
+        if bad_tax_list:
+            taxa_warning = "Bad taxa names found in the file provided: {}.Ignoring them.".format(
+                "\n".join(bad_tax_list))
+            flash(taxa_warning)
+    else:
+        taxa_error = "No valid taxa names or IDs found in the list or file provided: {}.Ignoring them.".format(
+            "\n".join(bad_tax_list))
+        error_list.append(taxa_error)
+    # debug("Converted tax list to tax ID files, saved new list in: {}".format(taxa_list_file))
+
+    new_value_list = [value_list[0], value_list[1], value_list[2], value_list[3], value_list[4], csv_path,
+                      taxa_list_file, origin_species, org_tax_id, value_list[8], value_list[9], value_list[10],
+                      value_list[11]]
+    return error_list, new_value_list
+
+
+def upload(request):
+    if request.method == 'POST':
+        uploaded_files = request.files.getlist("taxons")
+        print uploaded_files
+        uploaded_file = request.files['file']
+        if uploaded_file and allowed_file(uploaded_file.filename):
+            filename = secure_filename(uploaded_file.filename)
+            print filename
+            uploaded_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return
+
+
+# unique output page per user, displays one out of several options:
+# 1. error (+ go back button)
+# 2. success! check back soon, we also sent you an email
+@app.route('/output/<user_id>', methods=['POST'])
+# @app.route('/output', methods=['POST'])
+def handle_data(user_id):
+    email = (request.form['email'])
+    run_name = (request.form['run_name'])
+    reference_taxa = (request.form['reference_taxa'])
+
+    taxa_list = (request.form['taxa_list'])
+    taxa_list = taxa_list.split("\n")
+    taxa_list = [i.strip() for i in taxa_list]
+    path_to_taxa = utils.prepare_files(taxa_list, "taxons", user_id)
+
+    gene_list = (request.form['gene_list'])
+    gene_list = gene_list.split("\n")
+    gene_list = [i.strip() for i in gene_list]
+    path_to_genes = utils.prepare_files(gene_list, "genes", user_id)
+
+    # to do: FIND A WAY TO UPLOAD FILES~~
+    # upload(request)
+    # file = request.form.files['taxa_file']
+    # file.save(os.path.join(app.config['UPLOAD_FOLDER'], file))
+
+    evalue = (request.form['evalue'])
+    back_evalue = (request.form['back_evalue'])
+    identity = (request.form['identity'])
+    coverage = (request.form['coverage'])
+    string_similarity = (request.form['string_similarity'])
+
+    value_list = [float(evalue), float(back_evalue), int(identity), int(coverage), float(string_similarity),
+                  path_to_genes, path_to_taxa, reference_taxa, run_name, email, user_id, ip]
+    # message_list is a list of errors we need to pass back to the user
+    # if the list is empty, we move on to the run. else, we redirect use back to the form.
+    message_list, value_list = validate_data(value_list)
+
+    # message_list.append("You forgot something")
+    # message_list.append("Your file is not in the right format")
+
+    if message_list:  # means there are errors. later we might redirect the user to the form and
+        return render_template("server.html", errors=message_list, user_id=user_id)
+
+    elif not message_list:
+        success = send_job_to_backend(value_list)
+        if success:
+            flash('You successfully sent out a job!')
+            message = "Your job was sent to the server successfully! You will receive an email with a link" \
+                      "and you will be able to check the progress of your job"
+            utils.add_email_empty_file(email, user_id)
+            return render_template("page.html", message=message)
+        else:
+            error = "There was an unknown error with you data. Please try again: "
+            error += "\n\n Go back to the form and edit you data:<a href='server'>Home</a>"
+            return render_template("server.html", errors=error, user_id=user_id)
+
+    else:
+        error = "unkown error. try again"
+        return render_template("server.html", errors=error, user_id=user_id)
+
+
+@app.route('/results/<user_id>')
+def results(user_id):
+    return render_template('results.html')
+
+
+if __name__ == "__main__":
+    app.run()
---
+++ utils.py
@@ -0,0 +1,233 @@
+import os
+import tarfile
+from time import strftime, sleep
+import re
+import subprocess
+from Bio import Entrez
+import shutil
+
+TEMP_FILES_PATH = os.getcwd()  # TODO: change path for server
+
+
+def prepare_files(items, file_name, user_id):
+    full_path = os.path.join(TEMP_FILES_PATH, "_".join([user_id, file_name]))
+    with open(full_path, 'w') as f:
+        for item in items:
+            f.write(item + "\n")
+    return full_path
+
+
+def add_email_empty_file(email, run_id):
+    corr_email = str(email.replace('@', '.'))
+    full_email_path = os.path.join(TEMP_FILES_PATH, corr_email)
+    with open(full_email_path, 'w') as emailfile:
+        emailfile.write(run_id)
+    return True
+
+
+def check_if_email_exists(email):
+    corr_email = email.replace('@', '.')
+    if os.path.exists(os.path.join(TEMP_FILES_PATH, corr_email)):
+        return True  # exists
+    else:
+        return False
+
+
+def delete_email(email):
+    corr_email = email.replace('@', '.')
+    full_email_path = os.path.join(TEMP_FILES_PATH, corr_email)
+    if os.path.exists(full_email_path):
+        os.remove(full_email_path)
+        return True  # exists
+    else:
+        return False
+
+
+# debugging function
+def debug_s(debug_string, to_debug):
+    """
+    Receives a string and prints it, with a timestamp.
+    :param debug_string: a string to print
+    :param to_debug: boolean flag: True means print, False - ignore.
+    :return:
+    """
+    if to_debug:
+        print "DEBUG {0}: {1}".format(strftime('%H:%M:%S'), debug_string)
+
+
+def create_folder_if_needed(path):
+    """
+    Receives a path and creates a folder when needed (if it doesn't already exist).
+    """
+    if os.path.exists(path):
+        print "{} dir exists".format(path)
+    else:
+        print "{} dir does not exist. Creating dir.".format(path)
+        os.mkdir(path)
+
+
+def targz_list(archive_name, file_list):
+    """
+    Returns True after
+    :param archive_name:
+    :param file_list:
+    :return:
+    """
+    with tarfile.open(archive_name, "w:gz") as tar:
+        for file_name in file_list:
+            tar.add(file_name, arcname=os.path.basename(file_name))
+            os.remove(file_name)  # removing the sge file
+    return True
+
+
+def cleanup(path, fasta_path, first_blast_folder, second_blast_folder):
+    """
+    Performs tar and gzip on sets of files produced by the program.
+    Then deletes the files and folders.
+    :param path:  # the run_folder
+    :param fasta_path:  # the folder containing fasta files
+    :param first_blast_folder:  # the folder containing the first blast results
+    :param second_blast_folder:  # the folder containing the second blast results
+    :return:
+    """
+    # clean pickles:
+    pickles = [os.path.join(path, x) for x in os.listdir(path) if x[-2:] == '.p']
+    pickle_archive = os.path.join(path, "pickles.tar.gz")
+    if targz_list(pickle_archive, pickles):
+        # pickles cleaned
+        pass
+    # compress all fasta_path
+    fasta_files = [os.path.join(fasta_path, x) for x in os.listdir(fasta_path)]
+    fasta_archive = os.path.join(path, "fasta_archive.tar.gz")
+    if targz_list(fasta_archive, fasta_files):
+        shutil.rmtree(fasta_path)
+    # compress all first_blast
+    first_blast_files = [os.path.join(first_blast_folder, x) for x in os.listdir(first_blast_folder)]
+    first_blast_archive = os.path.join(path, "first_blast_archive.tar.gz")
+    if targz_list(first_blast_archive, first_blast_files):
+        shutil.rmtree(first_blast_folder)
+    # compress all second_blast (recursive)
+    second_blast_archive = os.path.join(path, "second_blast_archive.tar.gz")
+    with tarfile.open(second_blast_archive, "w:gz") as tar:
+        tar.add(second_blast_folder, arcname=os.path.basename(second_blast_folder))
+        shutil.rmtree(second_blast_folder)
+    return True
+
+
+def write_blast_run_script(command_line):
+    """Writing a blast run script, and giving it run permissions."""
+    script_path = "/tmp/blastp_run.sh"  # default script location
+    with open(script_path, 'w') as script:
+        script.write("#! /bin/tcsh\n")
+        script.write("# The script is designed to run the following blastp command from RecBlast\n")
+        script.write(command_line)
+        # run permissions for the script:
+    os.chmod(script_path, 0751)
+    return script_path
+
+
+def merge_two_dicts(x, y):
+    """Given two dicts, merge them into a new dict as a shallow copy."""
+    z = x.copy()
+    z.update(y)
+    return z
+
+
+def is_number(s):
+    """
+    The script determines if a string is a number or a text.
+    Returns True if it's a number.
+    """
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
+
+
+def blastdb_exit():
+    """Exiting if we can't find the $BLASTDB on the local machine"""
+    print("$BLASTDB was not found! Please set the blast DB path to the right location.")
+    print("Make sure blast+ is installed correctly.")
+    exit(1)
+
+
+def exists_not_empty(path):
+    """Receives a file path and checks if it exists and not empty."""
+    if os.path.exists(path) and os.stat(path).st_size > 0:
+        return True
+    else:
+        return False
+
+
+def subset_db(tax_id, gi_file_path, db_path, big_db, run_anyway, DEBUG, debug, attempt_no=0):  # TODO call it, doc it
+    """
+    Subsets a big blast database into a smaller one based on tax_id.
+    The function connects to entrez and retrieves gi identifiers of sequences with the same tax_id.
+
+    :param tax_id: The tax_id (string)
+    :param gi_file_path: file path of the gi_list file we are creating
+    :param db_path: the new db path
+    :param big_db: we are about to subset
+    :param run_anyway: run on NR if unable to subset
+    :param attempt_no: counter for the attempts in connecting to Entrez (attempts to connect up to 10 times).
+    :param DEBUG: A boolean flag: True for debug_func prints, False for quiet run.
+    :param debug: A function call to provide debug_func prints.
+    :return:
+    """
+
+    # connecting to ENTREZ protein DB
+    try:
+        handle = Entrez.esearch(db="protein", term="txid{}[ORGN]".format(tax_id), retmode="xml", retmax=10000000)
+        record = Entrez.read(handle)
+
+    except Exception, e:  # DB connection exception
+        print "Error connecting to server, trying again..."
+        print "Error: {}".format(e)
+        debug("Error connecting to server, trying again...\n")
+
+        # sleeping in case it's a temporary database problem
+        sleep_period = 180
+        print "restarting attempt in {} seconds...".format(sleep_period)
+        sleep(sleep_period)
+
+        # counting the number of attempts to connect.
+        attempt_no += 1
+        if attempt_no >= 10:  # If too many:
+            print "Tried connecting to Entrez DB more than 10 times. Check your connection or try again later."
+            exit(1)
+
+        # try again (recursive until max)
+        return subset_db(tax_id, gi_file_path, db_path, big_db, run_anyway, DEBUG, debug, attempt_no)
+
+    assert int(record["Count"]) == len(record["IdList"]), "Did not fetch all sequences!"  # make sure we got it all...
+    # writing a gi list file
+    with open(gi_file_path, 'w') as gi_file:
+        gi_file.write("\n".join(record["IdList"]) + "\n")
+
+    # the new target database path
+    create_folder_if_needed(os.path.join(db_path, tax_id))
+    target_db = os.path.join(db_path, tax_id, "db")
+    aliastool_command = ["blastdb_aliastool", "-gilist", gi_file_path, "-db", big_db, "-dbtype", "prot", "-out",
+                         target_db]
+    try:
+        subprocess.check_call(aliastool_command)
+        print("Created DB subset from nr protein for {}".format(tax_id))
+        return target_db
+    except subprocess.CalledProcessError:
+        print("Problem with creating DB for tax_id {} from nr.".format(tax_id))
+        if run_anyway:
+            print("Running with the heavy nr option. Do some stretches, it might be a long run.")
+            return big_db
+        print("Aborting.\n"
+              "If you want to run the program anyway against the entire nr "
+              "(which is significantly slower than the default run, please use the --run_even_if_no_db_found flag.")
+        exit(1)
+
+
+# for efficiency
+strip = str.strip
+split = str.split
+replace = str.replace
+re_search = re.search
+upper = str.upper
