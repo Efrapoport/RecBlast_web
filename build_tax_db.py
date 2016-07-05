becd1879b190af4df4353d50e551cf49335d0bbc
neuhofmo <neuhofmo@gmail.com>
2016-07-05 11:24:54 +0300

    added changes to build_tax_db from the web server Supposed to deal with the memory overload from loading redis

--- build_tax_db.py
+++ build_tax_db.py
@@ -4,18 +4,18 @@ from redis_pool import redis_pool
 from taxa import generate_tax_name_redis_key
 from RecBlastUtils import strip, split
 
-pipeline = redis_pool.pipeline()
+# pipeline = redis_pool.pipeline()  # changed later because of low memory on web server
 
 # doing it gradually and slowly to avoid memory overload
 with open('DB/tax_names.txt', 'rb') as f:
     for idx, line in enumerate(f.xreadlines()):
         value, key = split(strip(line), '\t')
 
-        pipeline.set(generate_tax_name_redis_key(key), value)
-        pipeline.set("tax.values.{}".format(value), key)
+        redis_pool.set(generate_tax_name_redis_key(key), value)
+        redis_pool.set("tax.values.{}".format(value), key)
 
         if idx % 100 == 0:
             print "Done: {}".format(idx)
 
-pipeline.execute()
+# redis_pool.execute()
 print "DONE!"
