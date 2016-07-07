cc6c51b3eaa7fcc74d3d433b1872d7243ae4b807
neuhofmo <neuhofmo@gmail.com>
2016-07-07 15:47:34 +0300

    exception for removing mail from redis

--- app.py
+++ app.py
@@ -78,10 +78,14 @@ def allowed_file(filename):
 def send_job_to_backend(value_list):
     """Send the job to queue."""
     try:
-        return queue.run_recblast_on_worker(value_list)
+        run_result = queue.run_recblast_on_worker(value_list)
+        return run_result
         # sending to the backend main
         # return True
-    except:
+    except Exception, e:
+        users.delete_email(value_list[10])
+        print("error: {}\nDeleting email.".format(e))
+        print("Unknown error. Please report to recblast@gmail.com.")
         return False
 
 
@@ -96,89 +100,94 @@ def validate_data(value_list):
     if users.has_job_for_email(email):
         error_list.append("There's already a job running for this email: %s" % email)
 
-    # check if files/lists are not empty and not too long:
-    gene_file_path = value_list[5]
-    max_allowed_genes = 10  # this is where we set the maximum allowed genes
-    if exists_not_empty(gene_file_path):
-        if file_len(gene_file_path) > max_allowed_genes:
-            error_list.append("Genes provided exceed the maximum number of allowed genes: {}".format(max_allowed_genes))
-    else:
-        error_list.append("No gene list or gene file provided!")
-
-    taxa_file = value_list[6]
-    max_allowed_taxa = 10  # this is where we set the maximum allowed taxa
-    if exists_not_empty(taxa_file):
-        if file_len(taxa_file) > max_allowed_taxa:
-            error_list.append("Genes provided exceed the maximum number of allowed taxa: {}".format(max_allowed_taxa))
-    else:
-        error_list.append("No taxa list or gene file provided!")
-
-    # validate reference taxa!
-    reference_taxa = value_list[7]
-    # processing original species
-    if is_number(reference_taxa):  # already a tax_id
-        org_tax_id = reference_taxa
-        try:
-
-            origin_species = get_name_by_value(org_tax_id).capitalize()
-        except KeyError:
-            error_list.append("Unknown tax id: {}!".format(org_tax_id))
-            origin_species = ""
-            # debug("Tax id given: {0}, which is {1}, no need to convert.".format(org_tax_id, origin_species))
-    else:  # it's a tax name
-        origin_species = reference_taxa.capitalize()
-        # convert it to tax id and write it to a file
-        try:
-            org_tax_id = get_value_by_name(origin_species)
-        except KeyError:
-            error_list.append("Unknown taxon name: {}!".format(origin_species))
-            org_tax_id = ""
-            # debug("Tax name given: {0}, which is tax_id {1}, converted. Saved original tax_id to {2}".format(
-            #     origin_species, org_tax_id, original_taxa_file))
-    # left with (org_tax_id, origin_species)
-
-    #   validate gene list
-    # parsing the genes_csv if it's a csv, and transforming it if it's a gene list file
-    is_csv = False  # by default
-    with open(gene_file_path, 'r') as gene_file:
-        for line in gene_file:
-            if line.find(',') != -1:
-                is_csv = True
-                break
-    if is_csv:
-        csv_path = gene_file_path  # hope it's a good and valid file...
-        # TODO: don't do this now
-        error_list.append("It's csv, we don't support it now!")
-    else:  # if the csv is not provided, create it from a gene file
-        if org_tax_id == "":
-            error_list.append("The gene_list could not be converted. Please check and upload a valid file.")
-            csv_path = ""
+    try:
+        # check if files/lists are not empty and not too long:
+        gene_file_path = value_list[5]
+        max_allowed_genes = 10  # this is where we set the maximum allowed genes
+        if exists_not_empty(gene_file_path):
+            if file_len(gene_file_path) > max_allowed_genes:
+                error_list.append("Genes provided exceed the maximum number of allowed genes: {}".format(max_allowed_genes))
         else:
-            conversion_result = csv_transformer.gene_file_to_csv(gene_file_path, org_tax_id)
-            if conversion_result[0]:
-                csv_path = conversion_result[1]  # quits if no input could be converted
-            else:
+            error_list.append("No gene list or gene file provided!")
+
+        taxa_file = value_list[6]
+        max_allowed_taxa = 10  # this is where we set the maximum allowed taxa
+        if exists_not_empty(taxa_file):
+            if file_len(taxa_file) > max_allowed_taxa:
+                error_list.append("Genes provided exceed the maximum number of allowed taxa: {}".format(max_allowed_taxa))
+        else:
+            error_list.append("No taxa list or gene file provided!")
+
+        # validate reference taxa!
+        reference_taxa = value_list[7]
+        # processing original species
+        if is_number(reference_taxa):  # already a tax_id
+            org_tax_id = reference_taxa
+            try:
+
+                origin_species = get_name_by_value(org_tax_id).capitalize()
+            except KeyError:
+                error_list.append("Unknown tax id: {}!".format(org_tax_id))
+                origin_species = ""
+                # debug("Tax id given: {0}, which is {1}, no need to convert.".format(org_tax_id, origin_species))
+        else:  # it's a tax name
+            origin_species = reference_taxa.capitalize()
+            # convert it to tax id and write it to a file
+            try:
+                org_tax_id = get_value_by_name(origin_species)
+            except KeyError:
+                error_list.append("Unknown taxon name: {}!".format(origin_species))
+                org_tax_id = ""
+                # debug("Tax name given: {0}, which is tax_id {1}, converted. Saved original tax_id to {2}".format(
+                #     origin_species, org_tax_id, original_taxa_file))
+        # left with (org_tax_id, origin_species)
+
+        #   validate gene list
+        # parsing the genes_csv if it's a csv, and transforming it if it's a gene list file
+        is_csv = False  # by default
+        with open(gene_file_path, 'r') as gene_file:
+            for line in gene_file:
+                if line.find(',') != -1:
+                    is_csv = True
+                    break
+        if is_csv:
+            csv_path = gene_file_path  # hope it's a good and valid file...
+            # TODO: don't do this now
+            error_list.append("It's csv, we don't support it now!")
+        else:  # if the csv is not provided, create it from a gene file
+            if org_tax_id == "":
                 error_list.append("The gene_list could not be converted. Please check and upload a valid file.")
                 csv_path = ""
+            else:
+                conversion_result = csv_transformer.gene_file_to_csv(gene_file_path, org_tax_id)
+                if conversion_result[0]:
+                    csv_path = conversion_result[1]  # quits if no input could be converted
+                else:
+                    error_list.append("The gene_list could not be converted. Please check and upload a valid file.")
+                    csv_path = ""
 
-    # validate taxa list
-    # converting taxa names list
-    (taxa_list_file, bad_tax_list, conversion_succeeded) = taxa_to_taxid.convert_tax_to_taxid(taxa_file)
-    if conversion_succeeded:
-        if bad_tax_list:
-            taxa_warning = "Bad taxa names found in the file provided: {}.Ignoring them.".format(
+        # validate taxa list
+        # converting taxa names list
+        (taxa_list_file, bad_tax_list, conversion_succeeded) = taxa_to_taxid.convert_tax_to_taxid(taxa_file)
+        if conversion_succeeded:
+            if bad_tax_list:
+                taxa_warning = "Bad taxa names found in the file provided: {}.Ignoring them.".format(
+                    "\n".join(bad_tax_list))
+                flash(taxa_warning)
+        else:
+            taxa_error = "No valid taxa names or IDs found in the list or file provided: {}.Ignoring them.".format(
                 "\n".join(bad_tax_list))
-            flash(taxa_warning)
-    else:
-        taxa_error = "No valid taxa names or IDs found in the list or file provided: {}.Ignoring them.".format(
-            "\n".join(bad_tax_list))
-        error_list.append(taxa_error)
-    # debug("Converted tax list to tax ID files, saved new list in: {}".format(taxa_list_file))
+            error_list.append(taxa_error)
+        # debug("Converted tax list to tax ID files, saved new list in: {}".format(taxa_list_file))
 
-    new_value_list = [value_list[0], value_list[1], value_list[2], value_list[3], value_list[4], csv_path,
-                      taxa_list_file, origin_species, org_tax_id, value_list[8], value_list[9], value_list[10],
-                      value_list[11]]
-    return error_list, new_value_list
+        new_value_list = [value_list[0], value_list[1], value_list[2], value_list[3], value_list[4], csv_path,
+                          taxa_list_file, origin_species, org_tax_id, value_list[8], value_list[9], value_list[10],
+                          value_list[11]]
+        return error_list, new_value_list
+    except Exception, e:  # if any error occurs:
+        users.delete_email(email)  # remove the emails
+        print("error: {}\nDeleting email.".format(e))
+        return ["Unknown error. Please report to recblast@gmail.com."], []
 
 
 def upload(request):
@@ -243,7 +252,7 @@ def handle_data():
 
     elif not message_list:
         # temp_email_string = "Someone sent a new job on RecBlast online!\n" \
-        #                     "email: {0}, run name: {1}, , run_id: {2}, species of origin: {3} (taxid: {4}), ip: {5}\n" \
+        #                 "email: {0}, run name: {1}, , run_id: {2}, species of origin: {3} (taxid: {4}), ip: {5}\n" \
         #                     "Started at: {6}".format(email, run_name, user_id, reference_taxa, value_list[8], user_ip,
         #                                              strftime('%H:%M:%S'))
         # new_value_list = ["recblast@gmail.com", run_name, user_id, temp_email_string]
